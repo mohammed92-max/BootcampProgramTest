738e8f9fe66e5080f8dfbf2729f12b788d8e0a7b
mohammed92-max <63492636+mohammed92-max@users.noreply.github.com>
2020-04-29 11:25:16 +0530

    ok

--- Helloworld.java
+++ Helloworld.java
@@ -2,10 +2,8 @@ public class Helloworld{
 	public static void main(String[] argv)
 	{
 		System.out.println("Hello world to coding club testing");
-<<<<<<< HEAD
+
 		System.out.println("goog night");
-=======
-		System.out.println("Good morning");
->>>>>>> a9151657e60dbeb5f67ce665a1dbfb122090b6f5
+
 	}
 }
