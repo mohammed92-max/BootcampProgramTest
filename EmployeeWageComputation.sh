263d1bfe72effc09cc8b9e49042e520b46df4954
Shahrukh <shahrukhkhandb@gmail.com>
2020-05-04 01:51:58 +0530

    pushing usecase 5

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,19 +1,40 @@
 #!/bin/bash -x
-isPartTime=1;
-isFullTime=2;
-employeeRatePerHour=20;
-empCheck=$((RANDOM%3));
-case $empCheck in
-        $isPartTime)
-                empHours=4
-                ;;
-        $isFullTime)
-                empHours=8
-                ;;
-        *)
-                empHours=0
-                ;;
-esac
-salary=$(($empHours * $employeeRatePerHour));
-echo $salary;
+#CONSTANTS FOR THE PROGRAM
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=40;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
+
+#VARIABLES
+totalEmpHrs=0;
+totalWorkingDays=0;
+
+function getWorkingHours() {
+        local $empCheck=$1
+        case $empCheck in
+                        $IS_FULL_TIME)
+                                empHrs=8
+                                ;;
+                        $IS_PART_TIME)
+                                empHrs=4
+                                ;;
+                        *)
+                                empHrs=0
+                                ;;
+        esac
+        echo $empHrs
+}
+
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+do
+        ((totalWorkingDays++))
+        empCheck=$((RANDOM%3));
+        empHrs="$(getWorkingHours $empCheck)"
+        totalEmpHrs=$(($totalEmpHrs+$empHrs))
+done
+
+totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR));
+echo "Total Employee Hours.." $totalEmpHrs
+echo "Total Employee salary.." $totalSalary
 
