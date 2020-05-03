4840ed5bc185702030ee1f87258818392c56e480
Shahrukh <shahrukhkhandb@gmail.com>
2020-05-04 00:43:56 +0530

    pushing usecase1

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,1 +1,15 @@
-echo "Welcome to Employee Wage Computation"
+#!/bin/bash -x
+randomCheck=$((RANDOM%2));
+isPresent=1;
+if [ $isPresent -eq $randomCheck ]
+then
+        echo "Employee is Present";
+else
+        echo "Employee is Absent";
+fi
+
+
+
+
+
+
