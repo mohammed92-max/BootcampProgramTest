544eb9547673cb0bbe4b04fc9a82c8da62061761
Shahrukh <shahrukhkhandb@gmail.com>
2020-05-04 01:31:36 +0530

    adding usecase 4

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,18 +1,19 @@
 #!/bin/bash -x
 isPartTime=1;
 isFullTime=2;
-EmployeeRatePerHour=20;
-randomCheck=$((RANDOM%3));
-if [ $isPartTime -eq $randomCheck ]
-then
-        workingHour=4;
-elif [ $isFullTime -eq $randomCheck ]
-then
-        workingHour=8;
-else
-        workingHour=0;
-fi
-salary=$(($workingHour * $EmployeeRatePerHour));
+employeeRatePerHour=20;
+empCheck=$((RANDOM%3));
+case $empCheck in
+        $isPartTime)
+                empHours=4
+                ;;
+        $isFullTime)
+                empHours=8
+                ;;
+        *)
+                empHours=0
+                ;;
+esac
+salary=$(($empHours * $employeeRatePerHour));
 echo $salary;
-
 
