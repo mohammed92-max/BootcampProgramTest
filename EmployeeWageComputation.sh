a67b00ba9aaac58453795c0c60508b28fef9272a
Shahrukh <shahrukhkhandb@gmail.com>
2020-05-04 10:11:21 +0530

    pushing UseCase6Day9

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,18 +1,19 @@
 #!/bin/bash -x
-#CONSTANTS FOR THE PROGRAM
+
 IS_PART_TIME=1;
 IS_FULL_TIME=2;
-MAX_HRS_IN_MONTH=40;
+MAX_HRS_IN_MONTH=10;
 EMP_RATE_PER_HR=20;
 NUM_WORKING_DAYS=20;
 
-#VARIABLES
 totalEmpHrs=0;
 totalWorkingDays=0;
 
-function getWorkingHours() {
-        local $empCheck=$1
-        case $empCheck in
+while [[ $totalEmpHr -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+do
+        ((totalWorkingDays++))
+        empCheck=$((RANDOM%3));
+                case $empCheck in
                         $IS_FULL_TIME)
                                 empHrs=8
                                 ;;
@@ -22,15 +23,7 @@ function getWorkingHours() {
                         *)
                                 empHrs=0
                                 ;;
-        esac
-        echo $empHrs
-}
-
-while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
-do
-        ((totalWorkingDays++))
-        empCheck=$((RANDOM%3));
-        empHrs="$(getWorkingHours $empCheck)"
+                esac
         totalEmpHrs=$(($totalEmpHrs+$empHrs))
 done
 
