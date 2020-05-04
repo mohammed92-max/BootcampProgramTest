486abccf8ed933923779ba98d2280cf75871a886
Shahrukh <shahrukhkhandb@gmail.com>
2020-05-04 10:38:30 +0530

    pushing usecase 9

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,14 +1,14 @@
-#!/bin/bash -x
-
 IS_PART_TIME=1;
 IS_FULL_TIME=2;
 MAX_HRS_IN_MONTH=40;
 EMP_RATE_PER_HR=20;
 NUM_WORKING_DAYS=20;
 
-
+#VARIABLES
 totalEmpHrs=0;
 totalWorkingDays=0;
+
+declare -A dailyWage;
 
 function getWorkingHours() {
         local empCheck=$1
@@ -34,13 +34,19 @@ echo $(($empHrs*$EMP_RATE_PER_HR))
 while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
         ((totalWorkingDays++))
- 	empCheck=$((RANDOM%3));
+        empCheck=$((RANDOM%3));
         empHrs="$(getWorkingHours $empCheck)"
         totalEmpHrs=$(($totalEmpHrs+$empHrs))
-        dailyWage[$totalWorkingDays]=$( getEmpWage $empHrs )
+        #day="Day"
+        dailyWage["Day "$totalWorkingDays]=$( getEmpWage $empHrs )
 done
 
 totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR));
 echo "Total Employee Hours.." $totalEmpHrs
 echo "Total Employee salary.." $totalSalary
+
+echo ${dailyWage[@]}
+echo ${!dailyWage[@]}
+
+
 
