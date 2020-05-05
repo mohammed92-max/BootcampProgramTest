9e8b4740781173b67aa7b3fd00ce1abe2905a311
mohammed92-max <63492636+mohammed92-max@users.noreply.github.com>
2020-05-05 08:38:34 +0530

    Deleted extra file

--- Today.java
+++
@@ -1,9 +0,0 @@
-public class Today
-{
-	public static void main (String[] args)
-	{
-		System.out.println("tomorrow world");
-		System.out.println("day after tomorrow world");
-		System.out.println("day day after tomorrow world");
-	}
-}
